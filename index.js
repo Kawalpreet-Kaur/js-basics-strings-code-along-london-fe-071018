c754329da16173c3945d7d4364405d9fd2540866
Kawalpreet Kaur <kawalpreet.kaur@hotmail.com>
2018-08-11 20:10:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 // Write your code in this file!
-let currentUser='ABCD XYZ';
+let currentUser='Abcd xyz';
 
-let welcomeMessage = 'Welcome to Flatbook, '+ currentUser + '!';
+currentUser=currentUser.toUpperCase();
+
+// let welcomeMessage = 'Welcome to Flatbook, '+ currentUser + '!';
+
+let welcomeMessage = 'Welcome to Flatbook,   ${currentUser}!';
 
 let excitedWelcomeMessage='WELCOME TO FLATBOOK, ' + currentUser + '!';
 
-let shortGreeting='Welcome, ' + currentUser.substring(0,1) + '!';
+// let shortGreeting='Welcome, ' + currentUser.substring(0,1) + '!';
+
+let shortGreeting='Welcome, ' + currentUser.charAt(0) + '!';
